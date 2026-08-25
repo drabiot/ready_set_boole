06ecb5308d1632734024e51f8f46d670977dc91e
drabiot <tchartie@student.42angouleme.fr>
2026-08-25 19:05:02 +0200

    Add more test in truth table to see with the CNF and NNF formula

--- src/ex04Tester.cpp
+++ src/ex04Tester.cpp
@@ -6,7 +6,7 @@
 /*   By: tchartie <tchartie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/20 17:25:53 by tchartie          #+#    #+#             */
-/*   Updated: 2026/08/20 20:49:53 by tchartie         ###   ########.fr       */
+/*   Updated: 2026/08/25 19:04:13 by tchartie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,12 @@ void	truthTableTester() {
 	PRINT RED "TRUTH TABLE TESTER" CENDL;
 	NLINE;
 	PRINT MAGENTA "   (A ∧ B) ∨ C" CENDL;
+	PRINT YELLOW "       RPN" CENDL;
 	print_truth_table("AB&C|");
+	PRINT YELLOW "       NNF" CENDL;
+	print_truth_table(negation_normal_form("AB&C|"));
+	PRINT YELLOW "       CNN" CENDL;
+	print_truth_table(conjonctive_normal_form("AB&C|"));
 	NLINE;
 	PRINT MAGENTA " (A ∧ B) ∨ A" CENDL;
 	print_truth_table("AB&A|");
